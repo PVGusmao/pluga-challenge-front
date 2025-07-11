9cbcca9b1b34d3d1ed476cb861c2b6cb84eeb337
Paulo <paulo.gusmao@sellbie.com.br>
2025-07-11 03:44:14 -0300

    refactor: implementado url direto no api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //url api
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_PLUGA_API_URL,
+  baseURL: "https://pluga.co/ferramentas_search.json",
 });
 
 export default api;
